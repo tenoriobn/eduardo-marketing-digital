cee32f94add97395cb6ad2f0e896930a98eca5ed
Bruno Tenório <tenoriobn@gmail.com>
2025-06-10 20:37:18 -0300

    test: :test_tube: 10/06/25 - valida renderização inicial e navegação por scroll nos links do menu

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,9 +1,32 @@
 describe('Home Page', () => {
-  it('should visit the home page', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
 
-    cy.findByRole('link', { name: 'Início' }).should('be.visible');
+  it('should start at the beginning of the landing page', () => {
+    cy.get('.active').contains('Início');
+    cy.get('#inicio').find('h2').should('exist').and('be.visible');
+  });
+
+  describe('Menu', () => {
+    beforeEach(() => {
+      cy.fixture('menuLinks').as('menuLinks');
+    });
+
+    it('should render menu links correctly', function () {
+      cy.get('nav').find('a').should('have.length', this.menuLinks.links.length);
+
+      this.menuLinks.links.forEach(({ label }) => {
+        cy.get('nav').contains('a', label).should('be.visible');
+      });
+    });
+
+    it('should scroll to the correct section on menu link click', function () {
+      this.menuLinks.links.forEach(({ label, url }) => {
+        cy.get('nav').contains('a', label).click();
+
+        cy.get(url).should('exist').and('be.visible');
+      });
+    });
   });
 });
-
-
